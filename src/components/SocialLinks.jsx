e978f07ba6376c8fff138d9594edb52f9185e3e4
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-01 19:29:05 +0530

    add portfolio link in resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -39,7 +39,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/Ramprasad_resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
